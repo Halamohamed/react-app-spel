6135abc17d6a0c3c9191156f91f455e89c983c52
Hala Mohammed <halaalimohammed@gmail.com>
2020-10-02 14:20:31 +0200

    first commit

---
+++ src/userContext.js
@@ -0,0 +1,24 @@
+
+
+/* enum choices{
+    Sten,
+    Sax,
+    Påse
+}
+
+interface UserInterface {
+    id: number,
+    name: string,
+    age: number,
+    choice: choices,
+} */
+
+ const User =   ()=> {
+    id: 1;
+    name: "Hala";
+    age: 34;
+    choices: choices.Påse;
+}
+
+export default User;
+
